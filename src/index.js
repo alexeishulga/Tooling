ad498ac3399631c91f789aa7f8cfe10544335dbe
alexeishulga <alexeishulg@gmail.com>
2022-02-27 18:56:09 +0300

    second step: setup css, image,svg

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
+import './style.css';
+import JS from './javascript.svg';
 
 function addElement() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello,', 'World'], ' ');
 
+  element.classList.add('image');
+  const Icon = new Image();
+  Icon.src = JS;
+  element.appendChild(Icon);
   return element;
 }
 
